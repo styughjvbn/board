3f6a930134b19a4df4c87098638132d298a7051a
JongWon <3338761@naver.com>
2022-10-03 19:03:28 +0900

    init commit

---
+++ src/main/java/com/board/board/hellocontroller.java
@@ -0,0 +1,12 @@
+package com.board.board;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hellocontroller {
+    @RequestMapping("/hello")
+    public String sayhello(){
+        return "hello";
+    }
+}
