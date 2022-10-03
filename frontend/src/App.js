e36f67eed81f6c35754292760ae4f91d27c2006d
JongWon <3338761@naver.com>
2022-10-04 00:30:40 +0900

    feat: 프론트엔드 리액트 추가

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+
+function App() {
+  const [message, setMessage]=useState([]);
+  useEffect(()=>{
+    fetch("/hello")
+        .then((res)=>{
+            console.log(res)
+          return res.text();
+        })
+        .then((data)=>{
+          setMessage(data);
+          console.log(data)
+        });
+  },[]);
+  return (
+    <div className="App">
+        <p>
+            {message}
+        </p>
+    </div>
+  );
+}
+
+export default App;
